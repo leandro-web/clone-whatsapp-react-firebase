4da741bac09c0b40d40d36b1bcc2785aafac552e
leandro-web <leandro-web@hotmail.com>
2021-06-26 12:23:13 -0300

    Subindo projeto

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDUVl-2fqrxuFp3Wifyx5c9P7ZH_otuM0w",
+    authDomain: "whatsappclone-cdad2.firebaseapp.com",
+    projectId: "whatsappclone-cdad2",
+    storageBucket: "whatsappclone-cdad2.appspot.com",
+    messagingSenderId: "564519067305",
+    appId: "1:564519067305:web:19b0ef1b4d9a2a398093b8",
+    measurementId: "G-PK1M131649"
+};
